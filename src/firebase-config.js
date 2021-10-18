ab19d6b112a962b0fdbd296d4e937aefb9996956
Mogakamo <mogaka.amo254@gmail.com>
2021-10-18 07:15:14 +0000

    firebase is correctly set

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+         apiKey: "AIzaSyADvG1DOHeXsIWkzGngvJgSU9D2fptNFBU",
+
+         authDomain: "vue-firebase-b147c.firebaseapp.com",
+
+         projectId: "vue-firebase-b147c",
+
+         storageBucket: "vue-firebase-b147c.appspot.com",
+
+         messagingSenderId: "793089472838",
+
+         appId: "1:793089472838:web:1ed8cd899a5dbc16f24d47",
+       };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
+import firebase from 'firebase/compat/app'
 import App from './App.vue'
+import { firebaseConfig } from './firebase-config.js'
+import 'firebase/compat/firestore'
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+
+db.collection('messages').get().then(r => {
+    r.docs.map(doc => {
+        console.log(doc.data())
+    })
+})
 
 createApp(App).mount('#app')
